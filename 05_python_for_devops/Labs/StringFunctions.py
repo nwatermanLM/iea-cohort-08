773a3d9e3c5fb5bb2804e68a9ff9bd338cd968dc
nwatpy <wkdoov@gmail.com>
2022-09-20 01:24:46 -0400

    string functions lab

--- 05_python_for_devops/Labs/StringFunctions.py
+++ 05_python_for_devops/Labs/StringFunctions.py
@@ -2,25 +2,76 @@
 # apply string stride to user input,
 # output alternatiing upper and lower case
 
-userIn = ""
-userStride = ""
-
-while userIn != str.isalpha:
+while True:
     print("Please type a phrase or group of letters: ")
     userIn = (input())
-    break
-else:
+    len(userIn) <= 26
+    if userIn.isalpha() == True:
+        userIn = userIn.lower()
+        print(f'You entered: {userIn}') 
+        break
     
-
-while userStride != int:
+while True:
     print("Please input a number between 1 - 4")
     userStride = int(input())
-    print("Input can only contain numbers: ")
-else:
-    pass
+    if userStride <= 4:
+        print(f'You entered: {userStride}')
+        break
 
-userIn = userIn.strip()
-for ltr in userIn:
-    print(ltr)
-    # if ltr == userIn[::userStride]:
-        # ltrdict
+Answer = []
+IncStride = 0
+ltrList = list(userIn)
+print(ltrList)
+
+while True:
+    IncStride <= userStride
+    for ltr in ltrList:
+        if ltr == ltrList[IncStride]:
+            Answer.append(ltr.upper())
+            del ltrList[IncStride]
+            IncStride += 1
+    print(ltrList)
+    print(Answer)
+
+# for ltr in ltrList[userStride]:
+
+
+    #Answer.append[IncStride]
+     # IncStride += 1
+
+# for ltr in userIn:
+    
+
+
+
+# UprIn = userIn[ZeroStride:userStride].upper()
+
+# print(userIn[ZeroStride:userStride].upper())
+
+# print(userIn[::userStride])
+
+# for ltr in userIn(StrideRange):
+    # print(ltr)
+
+
+# for ltr in userIn <= userIn[::userStride]:
+    # Uprltr = ltr
+     # print(Uprltr)
+
+
+
+# for ltr in userIn[::userStride].upper():
+    # UprLtr = ltr
+
+# for ltr in userIn[::userStride]:
+    # Lowltr = ltr
+
+# print(userIn.replace(Lowltr, UprLtr))
+
+    
+
+# UserStride = userIn[::userStride].upper()
+# StrideList = UserStride.split()
+# userInList = userIn.split()
+
+# userInList.replace([::userStride], 
