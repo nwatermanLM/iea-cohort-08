f18b46dfb380850cfb082271ef7dd60df0b62a65
nwatpy <nicholas.waterman@libertymutual.com>
2022-09-27 17:10:54 -0400

    9/27

--- 05_python_for_devops/Labs/convert_date-nwat.py
+++ 05_python_for_devops/Labs/convert_date-nwat.py
@@ -1,8 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import datetime
 import sys
+import argparse
 
 epoch = int(sys.stdin.read())
+local_conversion = datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S')
+iso_conversion = datetime.datetime.utcfromtimestamp(epoch).isoformat()
+parser = argparse.ArgumentParser(description= 'Convert current epoch time piped from bash')
+parser.add_argument('--iso', action='store_true', help= 'show as iso format')
+args = parser.parse_args()
 
-print(datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S'))
+if args.iso is True:
+    print(iso_conversion)
+else:
+    print(local_conversion)
+
+# def define_arguments()
+# return parser
+
+#def convert_date(epoch, args):
+# return  datestring
---
+++ 05_python_for_devops/Labs/convert_date2-nwat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import datetime
+import sys
+import argparse
+    
+if __name__ == "__main__":
+
+    epoch = int(sys.stdin.read())
+    parser = argparse.ArgumentParser(description= 'Convert current epoch time piped from bash')
+    parser.add_argument('--iso', action='store_true', help= 'show as iso format')
+    args = parser.parse_args()
+
+    def convert_date():
+        if args.iso is True:
+            return datetime.datetime.utcfromtimestamp(epoch).isoformat()
+        else:
+            return datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S')
+
+print(convert_date())
